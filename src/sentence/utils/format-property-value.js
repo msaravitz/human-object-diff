69c7b3c2c5c753a83ccd346d4fbfc799787c3c89
Matt Saravitz <mssaravitz@gmail.com>
2022-05-04 16:27:26 -0400

    support child property 2

--- src/sentence/utils/format-property-value.js
+++ src/sentence/utils/format-property-value.js
@@ -2,8 +2,9 @@ const format = require('date-fns/format');
 const isObject = require('../../diff/utils/is-object');
 
 function formatPropertyValue(value, config) {
+  console.log('config: ' + JSON.stringify(config, null, 2));
   if (typeof value === 'string') return `"${value}"`;
-  if (isObject(value)) return value[config.childProperty];
+  if (isObject(value)) return String(value[config.childProperty]);
   if (typeof value === 'number' || typeof value === 'boolean')
     return String(value);
   if (value instanceof Date) return `${format(value, config.dateFormat)}`;
